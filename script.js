c9c1ed9c963abdb12a8e3e08b718a054c801a18d
Harshal Kishor Bhadane <162085064+Harshal0117@users.noreply.github.com>
2025-03-22 15:13:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-//complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name; // Private variable for name
+        this._age = age;   // Private variable for age
+    }
 
-class Student extends Person {}
+    // Getter for name
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    // Getter for age
+    get age() {
+        return this._age;
+    }
+
+    // Setter for age
+    set age(age) {
+        this._age = age;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age); // Call the parent constructor
+    }
+
+    // Method to indicate the student is studying
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age); // Call the parent constructor
+    }
+
+    // Method to indicate the teacher is teaching
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
 
 // Do not change the code below this line
 window.Person = Person;
